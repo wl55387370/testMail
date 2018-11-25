dc296260b2ac1fff51c4ffbe6a772234aaeaefc9
wulin <15123552246@163.com>
2018-11-25 10:48:08 +0800

    自动发邮件 Signed-off-by: wulin <15123552246@163.com>

---
+++ src/main/java/com/testingedu/TestMail.java
@@ -0,0 +1,11 @@
+package com.testingedu;
+
+public class TestMail {
+
+	public static void main(String[] args) throws Exception {
+		
+		SendMail mail=new SendMail();
+        mail.send("测试测试测试");
+      
+
+}}
